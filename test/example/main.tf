f8e597d02be55a79aeb4a61feb3cf789ae47de96
owenmather <owenmather7@gmail.com>
2023-04-21 22:06:20 +0200

    Update main.tf

--- test/example/main.tf
+++ test/example/main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "sql_security_group" {
   description = "Security group for SQL Servers running on EC2"
-  vpc_id      = 122433840
+  vpc_id      = 1234567890
   tags        = ["app", "database"]
   name_prefix = var.prefix
 }
